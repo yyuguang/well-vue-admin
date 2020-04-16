877784b0301c5078ade4265c5e1e9a7f0a14aaf0
yyuguang <790335255@qq.com>
2020-04-16 20:50:55 +0800

    完善课程基本信息模块功能

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -117,10 +117,17 @@ export default {
     }
   },
   created() {
-    // 初始化所有讲师
-    this.getListTeacher()
-    // 初始化课程一级分类
-    this.getOneSubject()
+    // 获取路由id值
+    if (this.$route.params && this.$route.params.id) {
+      this.courseId = this.$route.params.id
+      // 调用根据id查询课程的方法
+      this.getInfo()
+    } else {
+      // 初始化所有讲师
+      this.getListTeacher()
+      // 初始化一级分类
+      this.getOneSubject()
+    }
   },
   methods: {
     // 上传封面成功调用的方法
@@ -166,6 +173,15 @@ export default {
         })
     },
     saveOrUpdate() {
+      // 判断添加还是修改
+      if (!this.courseInfo.id) {
+        // 添加
+        this.addCourse()
+      } else {
+        this.updateCourse()
+      }
+    },
+    addCourse() {
       course.saveCourseInfo(this.courseInfo)
         .then(response => {
           this.$message({
@@ -173,6 +189,45 @@ export default {
             message: '添加课程信息成功!'
           })
           this.$router.push({ path: '/course/chapter/' + response.data.courseId })
+        })
+    },
+    // 修改课程
+    updateCourse() {
+      course.updateCourseInfo(this.courseInfo)
+        .then(response => {
+          // 提示
+          this.$message({
+            type: 'success',
+            message: '修改课程信息成功!'
+          })
+          // 跳转到第二步
+          this.$router.push({ path: '/course/chapter/' + this.courseId })
+        })
+    },
+    // 根据课程id查询
+    getInfo() {
+      course.getOneById(this.courseId)
+        .then(response => {
+          // 在courseInfo课程基本信息，包含 一级分类id 和 二级分类id
+          this.courseInfo = response.data.courseInfoVo
+          // 1 查询所有的分类，包含一级和二级
+          subject.getSubjectList()
+            .then(response => {
+              // 2 获取所有一级分类
+              this.subjectOneList = response.data.list
+              // 3 把所有的一级分类数组进行遍历，
+              for (var i = 0; i < this.subjectOneList.length; i++) {
+                // 获取每个一级分类
+                var oneSubject = this.subjectOneList[i]
+                // 比较当前courseInfo里面一级分类id和所有的一级分类id
+                if (this.courseInfo.subjectParentId === oneSubject.id) {
+                  // 获取一级分类所有的二级分类
+                  this.subjectTwoList = oneSubject.children
+                }
+              }
+            })
+          // 初始化所有讲师
+          this.getListTeacher()
         })
     }
   }
